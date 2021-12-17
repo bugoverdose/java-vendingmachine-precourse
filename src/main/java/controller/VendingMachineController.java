5371c46f0115f9ef9bca91ff2b06f9003ce4e678
Jeong Satchel <bugod96@gmail.com>
2021-12-17 19:58:13 +0900

    feat: 자판기가 보유하고 동전을 출력

--- src/main/java/controller/VendingMachineController.java
+++ src/main/java/controller/VendingMachineController.java
@@ -3,11 +3,13 @@ package controller;
 import domain.CoinRepository;
 
 import static view.InputView.*;
+import static view.OutputView.*;
 
 public class VendingMachineController {
 
     public void run() {
         int money = Integer.parseInt(requestVendingMachineMoneyInput());
         CoinRepository.initCoins(money);
+        printVendingMachineCoins();
     }
 }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,20 @@
+package view;
+
+import static constants.OutputMessages.*;
+
+import domain.Coin;
+import domain.CoinRepository;
+
+public class OutputView {
+
+    public static void printVendingMachineCoins() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(VENDING_MACHINE_COINS);
+        for (Coin coin : Coin.values()) {
+            int number = CoinRepository.getCoins().get(coin);
+            sb.append(COIN_NUMBER_FORMAT(coin, number));
+        }
+        sb.append(BLANK_LINE);
+        System.out.println(sb);
+    }
+}
