4959d7e57b5da70dfd12862cce5a74ea7cf381af
Jeong Satchel <bugod96@gmail.com>
2021-12-17 21:40:03 +0900

    feat(StoredMoneyService): 자판기 보유 금액 입력시, 음수를 입력한 경우 예외로 간주

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -1,18 +1,25 @@
 package utils;
 
-import static constants.ExceptionMessages.NOT_INT_TYPE_EXCEPTION;
+import static constants.ExceptionMessages.*;
 
 public class InputValidator {
 
     public static void validateIntInput(String input) {
         int intInput = validateIntType(input);
+        validateNotNegative(intInput);
     }
 
-    public static int validateIntType(String input) {
+    private static int validateIntType(String input) {
         try {
             return Integer.parseInt(input);
         } catch (Exception e) {
             throw new IllegalArgumentException(NOT_INT_TYPE_EXCEPTION);
+        }
+    }
+
+    private static void validateNotNegative(int input) {
+        if (input < 0) {
+            throw new IllegalArgumentException(NEGATIVE_INT_EXCEPTION);
         }
     }
 }
