b13524357ff6704f2a9935e0cccd91e33a5a950d
Jeong Satchel <bugod96@gmail.com>
2021-12-17 21:42:21 +0900

    feat(StoredMoneyService): 자판기 보유 금액 입력시, 1원 단위에 값이 입력된 경우 예외로 간주

--- src/main/java/constants/ExceptionMessages.java
+++ src/main/java/constants/ExceptionMessages.java
@@ -5,4 +5,5 @@ public class ExceptionMessages {
     private static String ERROR = "[ERROR] ";
     public static String NOT_INT_TYPE_EXCEPTION = ERROR + "금액은 숫자여야 합니다.";
     public static String NEGATIVE_INT_EXCEPTION = ERROR + "금액은 음수일 수 없습니다.";
+    public static String ONE_WON_EXCEPTION = ERROR + "금액으로 1원 단위를 입력할 수 없습니다.";
 }
--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -1,12 +1,14 @@
 package utils;
 
 import static constants.ExceptionMessages.*;
+import static constants.SystemConstant.MINIMUM_COIN_AMOUNT;
 
 public class InputValidator {
 
     public static void validateIntInput(String input) {
         int intInput = validateIntType(input);
         validateNotNegative(intInput);
+        validateNoOneWon(intInput);
     }
 
     private static int validateIntType(String input) {
@@ -20,6 +22,12 @@ public class InputValidator {
     private static void validateNotNegative(int input) {
         if (input < 0) {
             throw new IllegalArgumentException(NEGATIVE_INT_EXCEPTION);
+        }
+    }
+
+    private static void validateNoOneWon(int input) {
+        if (input%MINIMUM_COIN_AMOUNT > 0) {
+            throw new IllegalArgumentException(ONE_WON_EXCEPTION);
         }
     }
 }
