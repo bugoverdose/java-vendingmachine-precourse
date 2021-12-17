0ccb92de2ea8e4c73f05224b68808eaff1107855
Jeong Satchel <bugod96@gmail.com>
2021-12-17 20:49:48 +0900

    feat(CustomerService): 투입 금액의 잔액으로 구매 가능한 상품이 남아있는지 확인

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,12 +1,28 @@
 package service;
 
+import domain.Product;
+import domain.ProductRepository;
+
 import static view.InputView.*;
 
 public class CustomerService {
 
     private int customerMoney = 0;
 
-    public void setCustomerMoney() {
+    public void run() {
+        setCustomerMoney();
+    }
+
+    private void setCustomerMoney() {
         this.customerMoney = Integer.parseInt(requestCustomerMoneyInput());
+    }
+
+    private boolean hasEnoughMoney() {
+        for (Product product : ProductRepository.getMenu()) {
+            if (product.getStock() == 0) continue;
+            if (product.getPrice() > customerMoney) continue;
+            return true;
+        }
+        return false;
     }
 }
